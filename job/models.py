8339d45469580541ef73943932ece2df84b3c4cf
Sunandan-FX <sunandandas131@gmail.com>
2025-10-23 01:31:50 +0600

    Update models.py

--- job/models.py
+++ job/models.py
@@ -11,6 +11,7 @@ class Job(models.Model):
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    # enter self for identify
     def __str__(self):
         return self.title
 
