cb40ea879eb6566c2d7abb60ca7b1535c476dae7
Anameeks <anamikadhar02@gmail.com>
2025-11-13 22:22:52 +0600

    Add files via upload

--- user/admin.py
+++ user/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import UserProfile
 
-admin.site.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role', 'address', 'github_link')
+    list_filter = ('role', 'user__date_joined')
+    search_fields = ('user__username', 'user__email', 'address')
+    readonly_fields = ('user',)
+    fieldsets = (
+        ('User Information', {
+            'fields': ('user',)
+        }),
+        ('Profile Details', {
+            'fields': ('role', 'profile_picture', 'address', 'github_link')
+        }),
+    )
+
+admin.site.register(UserProfile, UserProfileAdmin)
